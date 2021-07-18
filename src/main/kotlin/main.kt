96575de431838ec6d17422ef4708e6f89b2f832b
julioamorim <julio.l.amorim00@gmail.com>
2021-07-18 14:33:11 -0300

    trabalhando com fluxos condicionais novos (when)
    Working with when clause

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,7 +1,27 @@
 fun main() {
-    println("Hello World!")
-    val titular = "Júlio" // variable with val dont change de type
+    println("Welcome to Bytebank!")
+    val titular: String = "Júlio" // variable with val dont change de type
+    val numeroConta: Int = 1000
+    var saldoConta: Double = 0.0
+    saldoConta = 100 + 2.00
+    saldoConta += 100.00
 
     println("Titular $titular") // with string template, you dont need concatenate with + operator
+    println("número da conta $numeroConta")
+    println("Saldo da conta $saldoConta \n")
+
+    if (saldoConta > 0.0)
+        println("Conta é positiva")
+    else if (saldoConta == 0.0)
+        println("Conta esta zerada")
+    else
+        println("Conta negativa")
+
+    when {
+        saldoConta > 0.0 -> println("Conta é positiva")
+        saldoConta == 0.0 -> println("Conta esta zerada")
+        else -> println("Conta negativa")
+    }
+
 
 }
