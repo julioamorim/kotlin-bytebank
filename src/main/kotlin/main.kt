51129e6a92accd059bade883b2bb8310f8c8855a
julioamorim <julio.l.amorim00@gmail.com>
2021-07-18 04:24:35 -0300

    Printing value

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,7 @@
 fun main() {
-    print("Hello World!")
+    println("Hello World!")
+    val titular = "Júlio" // variable with val dont change de type
+
+    println("Titular $titular") // with string template, you dont need concatenate with + operator
+
 }
